c9a2f05deb45e5614406350825b49918861eca47
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-05-16 15:36:11 -0300

    Agrego método peek y pop pero falta el bloqueo

--- ListMonitor/src/model/ListMonitor.java
+++ ListMonitor/src/model/ListMonitor.java
@@ -1,15 +1,16 @@
 package model;
 
+import javax.print.attribute.standard.PrinterLocation;
+
 public class ListMonitor {
 
 	private int[] list;
 	private int cantInt;
 
 	public ListMonitor() {
-
 		this.cantInt = 0;
 		// TODO - Inicializar el array
-		// list = new int[3];
+		 list = new int[3];
 	}
 
 	public synchronized int size() {
@@ -21,7 +22,6 @@ public class ListMonitor {
 	}
 
 	public synchronized boolean contains(int elem) {
-
 		boolean contain = false;
 
 		for (int i = 0; i < list.length && !contain; i++) {
@@ -29,9 +29,25 @@ public class ListMonitor {
 		}
 		return contain;
 	}
+	
+	public synchronized int peek(){
+		//falta bloquear si no hay elementos
+		return list[0];
+	}
+	
+	public synchronized int pop(){
+		//falta bloquear si no hay elementos
+		int first = list[0];
+		int[] temp = list;
+		
+		for(int i = 0; i < temp.length - 1; i++){
+			list[i] = temp[i+1];
+		}
+		cantInt-=1;
+		return first;
+	}
 
 	public synchronized void add(int itgr) {
-
 		if (hayEspacio()) {
 			list[cantInt] = itgr;
 			cantInt++;
@@ -44,11 +60,9 @@ public class ListMonitor {
 			list[cantInt] = itgr;
 			cantInt++;
 		}
-
 	}
-
+	
 	private boolean hayEspacio() {
 		return cantInt < list.length;
 	}
-
 }
