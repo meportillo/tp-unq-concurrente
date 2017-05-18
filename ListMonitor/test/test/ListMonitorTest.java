7e8a0dbc2ad89b37c51247ac18fb9d6d787b361d
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-05-18 13:01:57 -0300

    Agrego test del método sort (Revisar)

--- ListMonitor/test/test/ListMonitorTest.java
+++ ListMonitor/test/test/ListMonitorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.PrintStream;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import model.ListMonitor;
@@ -19,38 +20,92 @@ public class ListMonitorTest {
 	}
 
 	@Test
-	public void testAlIniciarUnaListaMonitorElTamanioDebeSerCero() {
+	public void testAlIniciarUnaListMonitorElTamanioDebeSerCero() {
 		assertEquals(0, lista.size());
 	}
 
 	@Test
-	public void testAlIniciarUnaListaMonitorSeEncuentraVacia() {
+	public void testAlIniciarUnaListMonitorDebeEncontrarseVacia() {
 		assertTrue(lista.isEmpty());
 	}
 	
 	@Test
-	public void testSiAUnaListaMonitorSinElementosSeLePreguntaSiContieneXDevuelveFalso(){
+	public void testSiAUnaListMonitorVaciaSeLePreguntaSiContieneXDevuelveFalso(){
 		assertFalse(lista.contains(100));
 	}
 	
+	@Ignore
 	@Test
-	public void testElPeekAUnaListaMonitorConUnEnteroDevuelveEseEntero(){
+	public void testDelMetodoPeekAUnaListMonitorVacia(){
+		
+	}
+	
+	@Ignore
+	@Test
+	public void testDelMetodoPopAUnaListMonitorVacia(){
+		
+	}
+	
+	@Test
+	public void testPeekAUnaListMonitorConUnEnteroDevuelveEseEntero(){
 		lista.add(10);
 		assertEquals(10, lista.peek());
 	}
 	
 	@Test
-	public void testPopAUnaListaMonitorConUnEnteroDevuelveEseEntero(){
+	public void testPopAUnaListMonitorConUnEnteroDevuelveEseEntero(){
 		lista.add(10);
 		
-		assertTrue(10 == lista.pop());
+		assertEquals(10, lista.pop());
 	}
 	
 	@Test
-	public void testPopAUnaListaMonitorConUnEnteroDejaLaListaVacia(){
+	public void testPopAUnaListMonitorConUnEnteroDejaLaListaVacia(){
 		lista.add(10);
 		lista.pop();
 		
 		assertTrue(lista.isEmpty());
+	}
+	
+	@Test
+	public void testAgregarUnElementoAUnaListMonitorAumentaElTamanio(){
+		lista.add(10);
+		
+		assertEquals(1, lista.size());
+	}
+	
+	@Test
+	public void testSiSeAgregaUnElementoAUnaListMonitorYSeLePreguntaSiLoContieneEsTrue(){
+		lista.add(10);
+		
+		assertTrue(lista.contains(10));
+	}
+	
+	@Test
+	public void testMergeSort(){
+		lista.add(10);
+		lista.add(1);
+		lista.add(110);
+		lista.add(0);
+		lista.add(16);
+		lista.add(20);
+		lista.add(3);
+		lista.add(7);
+		lista.add(2);
+		
+		lista.sort();
+		
+		int n = lista.size();
+		int mayor = lista.peek();
+		boolean ordenada = true;
+		
+		for(int i= 0; i < n; i++){
+			if(lista.peek() >= mayor && ordenada){
+				mayor = lista.peek();
+				ordenada = ordenada && (lista.peek() >= mayor);
+				lista.pop();
+			}
+		}
+		assertTrue(ordenada);
 	}
 }
