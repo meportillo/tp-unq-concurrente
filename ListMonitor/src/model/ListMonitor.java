5d47be67005f0b2a7e1ad186fc299eae5fa618db
Ingrid Calderón <ingridgcalderon@gmail.com>
2017-05-18 00:50:11 -0300

    Agrego los métodos sort, merge y mergesort

--- ListMonitor/src/model/ListMonitor.java
+++ ListMonitor/src/model/ListMonitor.java
@@ -1,7 +1,5 @@
 package model;
 
-import javax.print.attribute.standard.PrinterLocation;
-
 public class ListMonitor {
 
 	private int[] list;
@@ -9,7 +7,6 @@ public class ListMonitor {
 
 	public ListMonitor() {
 		this.cantInt = 0;
-		// TODO - Inicializar el array
 		 list = new int[3];
 	}
 
@@ -24,7 +21,7 @@ public class ListMonitor {
 	public synchronized boolean contains(int elem) {
 		boolean contain = false;
 
-		for (int i = 0; i < list.length && !contain; i++) {
+		for (int i = 0; i < this.size() && !contain; i++) {
 			contain = contain || (list[i] == elem);
 		}
 		return contain;
@@ -40,10 +37,10 @@ public class ListMonitor {
 		int first = list[0];
 		int[] temp = list;
 		
-		for(int i = 0; i < temp.length - 1; i++){
+		for(int i = 0; i < temp.length-1; i++){
 			list[i] = temp[i+1];
 		}
-		cantInt-=1;
+		cantInt--;
 		return first;
 	}
 
@@ -61,6 +58,57 @@ public class ListMonitor {
 			cantInt++;
 		}
 	}
+	
+	/////////////////////////////////////////////////////////
+	//LO USO PARA TESTEAR EL METODO SORT. BORRAR
+	
+	
+	public int get(int i){
+		return list[i];
+	}
+	
+	/////////////////////////////////////////////////////////
+	
+    public void sort() {
+        mergesort(0, cantInt - 1);
+    }
+
+	private void mergesort(int low, int high) {
+        if (low < high) {
+        	int middle = low + (high - low) / 2;
+            mergesort(low, middle);
+            mergesort(middle + 1, high);
+            merge(low, middle, high);
+	    }
+	}
+
+    private void merge(int low, int middle, int high) {
+    	int[] aux = new int[cantInt];
+        for (int i = low; i <= high; i++) {
+            aux[i] = list[i];
+        }
+
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+        
+        while (i <= middle && j <= high) {
+            if (aux[i] <= aux[j]) {
+                list[k] = aux[i];
+                i++;
+            } else {
+                list[k] = aux[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= middle) {
+            list[k] = aux[i];
+            k++;
+            i++;
+        }
+    }
 	
 	private boolean hayEspacio() {
 		return cantInt < list.length;
